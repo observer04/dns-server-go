973b9ca142985ac070d8a502ee02153b5f0e4447
observer04 <tripathysarojinee@gmail.com>
2025-12-24 22:09:18 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -14,37 +14,38 @@ func main() {
 
 	// TODO: Uncomment the code below to pass the first stage
 	//
-	// udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	// if err != nil {
-	// 	fmt.Println("Failed to resolve UDP address:", err)
-	// 	return
-	// }
-	//
-	// udpConn, err := net.ListenUDP("udp", udpAddr)
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to address:", err)
-	// 	return
-	// }
-	// defer udpConn.Close()
-	//
-	// buf := make([]byte, 512)
-	//
-	// for {
-	// 	size, source, err := udpConn.ReadFromUDP(buf)
-	// 	if err != nil {
-	// 		fmt.Println("Error receiving data:", err)
-	// 		break
-	// 	}
-	//
-	// 	receivedData := string(buf[:size])
-	// 	fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	//
-	// 	// Create an empty response
-	// 	response := []byte{}
-	//
-	// 	_, err = udpConn.WriteToUDP(response, source)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to send response:", err)
-	// 	}
-	// }
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")  // reason for resolveudpaddr is to create a udp address structure ex: &UDPAddr{IP: net.IPv4(127,0,0,1), Port: 2053}
+	if err != nil {
+		fmt.Println("Failed to resolve UDP address:", err)
+		return
+	}
+	
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to address:", err)
+		return
+	}
+	defer udpConn.Close()
+	
+	buf := make([]byte, 512)
+	
+	
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+	
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+	
+		// Create an empty response
+		response := []byte{}
+	
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
 }
